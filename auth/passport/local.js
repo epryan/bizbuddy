50e1f3002819aab542d3fedaf9a0f8b6e5942b30
epryan <ryan@ryansip.com>
2018-02-14 03:15:23 -0800

    Made username checks case insensitive

--- auth/passport/local.js
+++ auth/passport/local.js
@@ -8,7 +8,7 @@ module.exports = new LocalStrategy({
     //passReqToCallback: true
     },
     function(username, password, done) {
-      User.findOne({'username': username}, 'username password', function(err, user) {
+      User.findOne({'username': username.toLowerCase()}, 'username password', function(err, user) {
         if (err) { return done(err); }
 
         if (!user) {
